da32354dd1c497a94c7d7633165956999789dc3b
Nicolaes Tollenaar <nico.tollenaar@gmail.com>
2022-03-09 06:45:00 +0100

    added websocket script to listen to blockchain events, now to add transaction model plus sse

--- src/context/backendUrl.context.js
+++ src/context/backendUrl.context.js
@@ -1,13 +1,14 @@
-import "dotenv/config";
+// import "dotenv/config";
 
 import { createContext, useState } from "react";
 
 const BackendUrlContext = createContext();
 
 function BackendUrlProviderWrapper(props) {
-  const [backendUrl, setBackEndUrl] = useState(
-    process.env.CHAINACCOUNT_API_URL
-  );
+  // const [backendUrl, setBackEndUrl] = useState(
+  //   process.env.CHAINACCOUNT_API_URL
+  // );
+  const [backendUrl, setBackEndUrl] = useState("http://localhost:4001");
 
   return (
     <BackendUrlContext.Provider value={backendUrl}>
